60e7b4552a8c7e7bf353034907227575e8455886
StrunkGroove <117349896+StrunkGroove@users.noreply.github.com>
2023-09-26 17:47:04 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -102,7 +102,6 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/0",
         "LOCATION": (
             f'redis://{DEFAULT_REDIS_HOST}'
             f':{DEFAULT_REDIS_PORT}/{DEFAULT_REDIS_NUMBER}'
