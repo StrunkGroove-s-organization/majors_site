8004585a9649674bffd26520acd1d0e54c0c71f8
StrunkGroove <117349896+StrunkGroove@users.noreply.github.com>
2023-10-06 08:12:49 +0300

    Delete block_spreadtable directory

--- block_spreadtable/views.py
+++
@@ -1,9 +0,0 @@
-from base.views import BaseFormView
-
-
-APP_NAME_URL = __package__ + '/'
-
-
-class BlockSpreadTaleView(BaseFormView):
-    url = APP_NAME_URL
-    template_name = 'block_spreadtable.html'
