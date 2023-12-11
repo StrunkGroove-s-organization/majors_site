82dce1286a698fa0d197811fd842b4d1e58ff5f2
StrunkGroove <jmarkj@mail.ru>
2023-12-12 01:18:20 +0300

    repair best-prices

--- app/price/views.py
+++ app/price/views.py
@@ -1,6 +1,10 @@
+import requests
+
 from django.core.cache import caches
 
+from rest_framework import status
 from rest_framework import permissions
+from rest_framework.response import Response
 
 from .serializers import PriceSerializer
 from base.views import BaseFormView, BaseAPIView
@@ -22,8 +26,16 @@ class BestPricesView(BaseFormView):
 class PriceView(BaseAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_serializer(self, data):
-        return PriceSerializer(data=data)
+    def post(self, request):
+        serializer = PriceSerializer(data=request.data)
+        
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        url = 'http://188.120.227.131:8001/api/v1/best-prices/'
+        response = requests.post(url, json=serializer.validated_data)
+        return Response({'data': response.json()})
+    
 
     def process_request(self, request, validated_data):
         token = validated_data.get('token')
