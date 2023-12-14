0cb94f01f939f78a2c9f586dd0dfc6c9870d2492
StrunkGroove <jmarkj@mail.ru>
2023-12-15 01:04:29 +0300

    fix

--- app/myproject/urls.py
+++ app/myproject/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('main.urls')),
@@ -19,4 +19,4 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-] # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
