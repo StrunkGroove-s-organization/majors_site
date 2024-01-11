15b02d908e60f675ec33a68f08dd6c6ba7d7b4e5
StrunkGroove <jmarkj@mail.ru>
2024-01-11 20:11:14 +0300

    add referral in profile

--- app/user_profile/urls.py
+++ app/user_profile/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('profile/', views.profile_view, name='profile'),
+    path('profile/', views.ReferralView.as_view(), name='profile'),
 ]
